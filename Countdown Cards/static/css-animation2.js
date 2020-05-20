8124cd5fd881a09a91489d74bd82e3c0dbc7636a
bigtoerag <subscribe@marcusandzoe.com>
2020-05-21 03:21:12 +1000

    Its working ya'll

--- Countdown Cards/static/css-animation2.js
+++ Countdown Cards/static/css-animation2.js
@@ -1,49 +1,24 @@
 // Original JavaScript code by Chirp Internet: www.chirp.com.au
 // Please acknowledge use of this code by including this header.
 
+
 var CardGame = function(targetId)
 {
   // private variables
   var cards = []
-
-  var card_value = ["1C","2C","3C","4C","5C","6C","7C","8C","9C","10C","1H","2H","3H","4H","5H","6H","7H","8H","9H","10H"];
-  var numbers = [ '&#x1F0D1', '&#x1F0D2', '&#x1F0D3', '&#x1F0D4', '&#x1F0D5', '&#x1F0D6', '&#x1F0D7', '&#x1F0D8', '&#x1F0D9', '&#x1F0DA','&#x1F0B1', '&#x1F0B2', '&#x1F0B3', '&#x1F0B4', '&#x1F0B5', '&#x1F0B6', '&#x1F0B7', '&#x1F0B8', '&#x1F0B9' ];
-  var tot_cards = 18;
+  var card_value = ["1C","2C","3C","4C","5C","6C","7C","8C","9C","10C","11C","12C","13C","1H","2H","3H","4H","5H","6H","7H","8H","9H","10H","11H","12H","13H","1S","2S","3S","4S","5S","6S","7S","8S","9S","10S","11S","12S","13S","1D","2D","3D","4D","5D","6D","7D","8D","9D","10D","11D","12D","13D"];
+  var numbers = [ '&#x1F0D1', '&#x1F0D2', '&#x1F0D3', '&#x1F0D4', '&#x1F0D5', '&#x1F0D6', '&#x1F0D7', '&#x1F0D8', '&#x1F0D9', '&#x1F0DA', '&#x1F0DB', '&#x1F0DD', '&#x1F0DE','&#x1F0B1', '&#x1F0B2', '&#x1F0B3', '&#x1F0B4', '&#x1F0B5', '&#x1F0B6', '&#x1F0B7', '&#x1F0B8', '&#x1F0B9', '&#x1F0BA', '&#x1F0BB', '&#x1F0BD', '&#x1F0BE','&#x1F0A1', '&#x1F0A2', '&#x1F0A3', '&#x1F0A4', '&#x1F0A5', '&#x1F0A6', '&#x1F0A7', '&#x1F0A8', '&#x1F0A9', '&#x1F0AA', '&#x1F0AB', '&#x1F0AD', '&#x1F0AE','&#x1F0C1', '&#x1F0C2', '&#x1F0C3', '&#x1F0C4', '&#x1F0C5', '&#x1F0C6', '&#x1F0C7', '&#x1F0C8', '&#x1F0C9', '&#x1F0CA', '&#x1F0CB', '&#x1F0CD', '&#x1F0CE' ];
+  var total_cards = card_value.length;
   var started = false;
   var matches_found = 0;
   var card1 = false, card2 = false;
+
 
   var hideCard = function(id) // turn card face down
   {
     cards[id].innerHTML = "&#x1F0A0";
     with(cards[id].style) {
-      transform = "scale(1.0)";
-    }
-  };
-
-  var moveToPack = function(id) // move card to pack
-  {
-    hideCard(id);
-    cards[id].matched = true;
-    cards[id].innerHTML = "&#x1F0CF"
-    with(cards[id].style) {
-      //zIndex = "1000";
-      //top = "100px";
-      //left = "-140px";
-      //transform = "rotate(0deg)";
-      //zIndex = "0";
-      display = "hidden";
-    }
-  };
-
-  var moveToPlace = function(id) // deal card
-  {
-    cards[id].matched = false;
-    with(cards[id].style) {
-      zIndex = "1000";
-      //transform = "rotate(" + (Math.floor(Math.random() * 5) + 178) + "deg)";
-      //zIndex = "0";
-
+    transform = "scale(1.0)";
     }
   };
 
@@ -51,8 +26,7 @@ var CardGame = function(targetId)
   {
     if(id === card1) return;
     if(cards[id].matched) return;
-    cards[id].innerHTML = shuffledCards[cards[id].id];
-    console.log(cards[id]);
+    cards[id].innerHTML = cards[id].getAttribute('cardunicode');
     with(cards[id].style) {
       // Firefox doesn't know which way to rotate
       if(matches = transform.match(/^rotate\((\d+)deg\)$/)) {
@@ -68,14 +42,16 @@ var CardGame = function(targetId)
 
     if(card1 !== false) {
       card2 = id;
-      if(parseInt(card_value[card1]) == parseInt(card_value[card2])) { // match found
+      if(parseInt(card_value[cards[card1].id]) == parseInt(card_value[cards[card2].id])) { // match found
         (function(card1, card2) {
-          setTimeout(function() { moveToPack(card1); moveToPack(card2); }, 1000);
+          setTimeout(function() { cardMatched(card1); cardMatched(card2); }, 600);
         })(card1, card2);
-        if(++matches_found == 8) { // game over, reset
+        if(++matches_found == total_cards/2) { // game over, reset
           matches_found = 0;
           started = false;
+          alert("Well Done");
         }
+
       } else { // no match
         (function(card1, card2) {
           setTimeout(function() { hideCard(card1); hideCard(card2); }, 800);
@@ -84,6 +60,7 @@ var CardGame = function(targetId)
       card1 = card2 = false;
     } else { // first card turned over
       card1 = id;
+
     }
   };
 
@@ -92,17 +69,19 @@ var CardGame = function(targetId)
     if(started) {
       showCard(id);
     } else {
-      // shuffle and deal cards
-      //card_value.sort(function() { return Math.round(Math.random()) - 0.5; });
-      //for(i=0; i < 18; i++) {
-      //  (function(idx) {
-      //    setTimeout(function() { moveToPlace(idx); }, idx * 100);
-      //  })(i);
-      //}
       started = true;
+			showCard(id);
     }
   };
-
+  var cardMatched = function(card)
+  {
+    //disable clocking on the card
+    cards[card].style.pointerEvents = 'none';
+    //set the card to the joker image
+    cards[card].innerHTML = "&#x1F0DF";
+    //disable the transform on the card
+    cards[card].style.transform = "scale(1)";
+  }
   // initialise
   var shuffle = function (array) {
 
@@ -134,19 +113,17 @@ var CardGame = function(targetId)
   // template for card
   var card = document.createElement("div");
   card.innerHTML = "&#x1F0A0";
-
-  for(var i=0; i < tot_cards; i++) {
+  var shuffledCards = shuffle(numbers.slice());
+  for(var i=0; i < total_cards; i++) {
     var newCard = card.cloneNode(true);
-    //newCard.innerHTML = numbers[i];
-    var shuffledCards = shuffle(numbers.slice());
     newCard.id = numbers.indexOf(shuffledCards[i]);
-    //newCard.fromtop = 15 + 120 * Math.floor(i/6);
-    //newCard.fromleft = 70 + 100 * (i%6);
+    newCard.setAttribute("cardunicode", shuffledCards[i]);
     (function(idx) {
       newCard.addEventListener("click", function() { cardClick(idx); }, false);
     })(i);
     started = true;
     felt.appendChild(newCard);
     cards.push(newCard);
+
   }
 }
